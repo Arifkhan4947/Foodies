ea494b7f4ee02439cedb09829842d4e3acb94e5f
Arif Khan <khanarif2738@gmail.com>
2024-09-18 23:43:13 +0530

    Header + Style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Home.css"
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
     <div>
-        This is home page
+        <Header/>
     </div>
   )
 }
